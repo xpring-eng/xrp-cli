f5da72df2999827630879fdd26dcc18642137454
Hans Bergren <hansbergren@gmail.com>
2020-01-08 14:33:41 -0500

    Add linting & auto-formatting to the project
    - Add ESLint & Prettier
    - Lint and format the basic CLI app
    - Add a package.json script for linting

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'airbnb-base',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',          // Disable ESLint rules that would conflict with prettier
+        'plugin:prettier/recommended',          // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
+    parserOptions: {
+        ecmaVersion: 2019,                      // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module',                   // Allows for the use of imports
+    },
+    rules: {
+        '@typescript-eslint/camelcase': 'off',  // I prefer snake_case variable names
+        'no-console': 'off',                    // It's a CLI, we need to log to the console
+    }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
+import * as commander from 'commander';
 
-const xrp_cli = require('commander');
+const xrp_cli = new commander.Command();
+
 xrp_cli.version('0.0.1');
 
 console.log('Welcome to the XRP CLI!');
